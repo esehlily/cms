fdb94cd763e1ce048aaa0524767459c14bd9cfa5
esehlily <esehlily@gmail.com>
2025-04-06 02:04:46 +0100

    first commit

---
+++ resources/views/auth/auth-layout/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }} - @yield('title')</title>
+    <link rel="stylesheet" href="{{asset('landing-assets/css/landing.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"                  integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="">
+        @yield('content')
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+</body>
+</html>
---
+++ resources/views/auth/auth-pages/landingpage/landing.blade.php
@@ -0,0 +1,23 @@
+@extends('auth.auth-layout.index')
+
+@section('title')
+    Landing
+@endsection
+
+@section('content')
+    <div class="d-flex vh-100">
+        <div class="col-lg-6 bg-white">
+            <div class="overall">
+                <h1 class="text1" style="line-height: 60px">Welcome to the <br> Complaint Portal</h1>
+                <p class="pt-3" style="line-height: 30px">Lay your complaints here whether it is relating to personal concern, environmental concern or just any complaints</p>
+                <div class="pt-4">
+                    <button class="login1">Login</button>
+                    <button class="register1">Register</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 bluey">
+            <img src="landing-assets\img\image.svg" alt="" class="picturey">
+        </div>
+    </div>
+@endsection
