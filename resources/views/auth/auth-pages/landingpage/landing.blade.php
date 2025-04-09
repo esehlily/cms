547a38db2d70fb8c8c7006c94073ce1d23862020
esehlily <esehlily@gmail.com>
2025-04-09 23:04:25 +0100

    second commit

--- resources/views/auth/auth-pages/landingpage/landing.blade.php
+++ resources/views/auth/auth-pages/landingpage/landing.blade.php
@@ -7,11 +7,14 @@
 @section('content')
     <div>
         <div class="row vh-100">
+            <div class="d-lg-none" style="width: 350px; margin: 0 auto; height: 300px;  ">
+                <img src="landing-assets\img\welcome image.svg" alt="" width="300px" height="350px">
+            </div>
             <div class="col-lg-6 bg-white">
                 <div class="overall">
-                    <h1 class="text1" style="line-height: 60px">Welcome to the <br> Complaint Portal</h1>
-                    <p class="pt-3" style="line-height: 30px">Lay your complaints here whether it is relating to personal concern, environmental concern or just any complaints</p>
-                    <div class="pt-4">
+                    <h1 class="text1 mb-0">Welcome to the <br> Complaint Portal</h1>
+                    <p class="parry mb-0">Lay your complaints here whether it is relating to personal concern, environmental concern or just any complaints</p>
+                    <div class="buttony">
                         <button class="login1">Login</button>
                         <button class="register1">Register</button>
                     </div>
